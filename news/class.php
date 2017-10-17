470a0442cbff5093ef15fafd7fb485073da0fdac
atham98 <1998atham@gmail.com>
2017-10-17 19:39:31 +0500

    Update class.php

--- news/class.php
+++ news/class.php
@@ -14,16 +14,5 @@ class News
 		$this->content = $content; 
 		$this->source = $source;
 	}
-	public function getTitle(){
-		return $this->title;
-	}
-	public function getDate(){
-		return $this->date;
-	}
-	public function getContent(){
-		return $this->content;
-	}
-	public function getSource(){
-		return $this->source;
-	}
+	
 }
